c63e06d222cf0259ddd0de8ab4fcc98c824a1aca
whynotee <saidbogota669@gmail.com>
2024-06-23 15:30:40 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
     app: {
-        px: '+',
-        token: '',
-        owners: '898999050320363550',
-        funny: '898999050320363550',
-        color: 'RANDOM',
-        footer: 'salahDev',
+        px: '.',
+        token: 'MTEzNDA0MDc1ODE0MDI4OTA1NA.GSVmE3.SMY0A-mtB8QW_VLHngEljFDMULt-ZWbYuM1AkE',
+        owners: '1077231084053463101',
+        funny: '',
+        color: '000000',
+        footer: 'marmelade',
         maxserver: '6',
         everyoneMention: false,
         hostedBy: true,
